0027ece9b9c68ff01a688aabbc5cf1f2e4744bbf
shalushahma <98680442+shalushahma@users.noreply.github.com>
2022-04-14 13:03:21 +0300

    ab

---
+++ parallaxmenu/script.js
@@ -0,0 +1,22 @@
+// theme
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+// nav
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+// function menuToggle() {
+//   let x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+// const hamburger = document.getElementById('hamburger');
+// hamburger.addEventListener('click', menuToggle);
